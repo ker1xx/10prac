2bd06e2dd454875913a419430aee2f96345cb84f
ker1xx <klinkamama@gmail.com>
2022-12-13 18:59:12 +0300

    yo

---
+++ 10prac/ICrud.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10prac
+{
+    internal interface ICrud
+    {
+        public void Create()
+        {
+
+        } 
+        public void Read()
+        {
+
+        } 
+        public void Update()
+        {
+
+        } 
+        public void Delete()
+        {
+
+        }
+    }
+}
---
+++ 10prac/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10prac
+{
+    internal class Person
+    {
+        public Person(string a, int b, int c)
+        {
+            name = a;
+            password = b;
+            enum_job_title = c;
+        }
+        public string name;
+        public int password;
+        public int enum_job_title;
+    }
+}
